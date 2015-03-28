e47b00e31e95b8c6aa6880d987271f9f0b3038b9
maltebargholz <maltebargholz@gmail.com>
2015-03-28 11:21:55 +0100

    [AppDelegate] —modified Tutorial Steps to include both the UIBarButtonItem and the standard nextStep button [ExampleViewController] — added a top Toolbar/Navigation bar (toggle between them with a bool)

--- Example/TutorialKitExample/ExampleViewController.m
+++ Example/TutorialKitExample/ExampleViewController.m
@@ -12,6 +12,7 @@
 @interface ExampleViewController()
 @property (nonatomic, weak) UIButton *nextButton;
 @property (nonatomic, weak) UIButton *startButton;
+@property (nonatomic, weak) UIView *topBar;
 @end
 @implementation ExampleViewController
 
@@ -20,6 +21,25 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor colorWithPatternImage:self.repeatingBackground];
+    
+    BOOL displayToolbar = YES;
+    UIBarButtonItem *theItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCamera target:self action:@selector(nextStep:)];
+    theItem.tag = 1001;
+    
+    if (displayToolbar) {
+        UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectZero];
+        [toolbar setItems:@[theItem]];
+        [self.view addSubview:toolbar];
+        _topBar = toolbar;
+    }
+    else{
+        UINavigationBar *navBar = [[UINavigationBar alloc] initWithFrame:CGRectZero];
+        UINavigationItem *navItem = [[UINavigationItem alloc] initWithTitle:@"Navigation Bar"];
+        [navItem setLeftBarButtonItem:theItem];
+        [navBar setItems:@[navItem]];
+        [self.view addSubview:navBar];
+        _topBar = navBar;
+    }
     
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
     btn.frame = CGRectMake(0, 0, 200.f, 60.f);
@@ -35,12 +55,12 @@
     // a reset button
     btn = [UIButton buttonWithType:UIButtonTypeCustom];
     btn.frame = CGRectMake(0, 0, 200.f, 60.f);
-    [btn setTitle:@"NEXT STEP" forState:UIControlStateNormal];
+    [btn setTitle:@"LAST STEP" forState:UIControlStateNormal];
     [btn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
     [btn setTitleColor:UIColor.blackColor forState:UIControlStateHighlighted];
     btn.backgroundColor = [UIColor colorWithRed:0.8 green:0.3 blue:0.3 alpha:1.0];
     btn.layer.cornerRadius = 15.f;
-    btn.tag = 1001;
+    btn.tag = 1002;
     [btn addTarget:self action:@selector(nextStep:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
     self.nextButton = btn;
@@ -57,7 +77,7 @@
         center.x = self.view.center.y;
         center.y = self.view.center.x;
     }
-    
+    self.topBar.frame = CGRectMake(0, 20, self.view.frame.size.width,44);
     self.startButton.center = CGPointMake(center.x, center.y * 0.5);
     self.nextButton.center = CGPointMake(center.x, self.startButton.center.y + 100.f);
 }
